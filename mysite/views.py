34fbe1d446c03e19e647ed40396ef1ec102b7f41
Jacob Preston <jacobpreston4@gmail.com>
2013-10-10 21:57:38 -0400

    hello

--- mysite/views.py
+++ mysite/views.py
@@ -1,4 +1,11 @@
 from django.http import HttpResponse
+import datetime
+
 
 def hello(request):
 	return HttpResponse("Hello world")
+
+def current_datetime(request):
+	now = datetime.datetime.now()
+	html = "<html><body>hello world<p>It is now %s. </p></body></html>" % now
+	return HttpResponse(html)
